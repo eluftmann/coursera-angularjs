0b4bd6267fc129d5f5be6b9aee4deaabd5c75469
Eryk Luftmann <eluftmann@gmail.com>
2016-10-01 04:46:04 +0200

    Added solution for assignment 1

---
+++ assignment1-solution/scripts/app.js
@@ -0,0 +1,33 @@
+(function () {
+    "use strict";
+
+    angular
+        .module("LunchCheck", [])
+        .controller("LunchCheckController", LunchCheckController);
+
+
+    LunchCheckController.$inject = ["$scope"];
+
+    function LunchCheckController($scope) {
+        var LUNCH_MENU_ITEMS_LIMIT = 3;
+
+        $scope.lunchMenu = "";
+        $scope.message = "";
+        $scope.validationStateClass = "";
+
+        $scope.checkLunchMenu = function () {
+            // Filter out empty items (e.g. ", ," or ",,") from `lunchMenu`
+            var lunchMenuArray = $scope.lunchMenu.split(",")
+                                                 .filter(function (item) { return item.trim(); });
+
+            if (lunchMenuArray.length === 0) {
+                $scope.message = "Please enter data first";
+                $scope.validationStateClass = "has-error";
+                return;
+            }
+
+            $scope.message = (lunchMenuArray.length <= LUNCH_MENU_ITEMS_LIMIT) ? "Enjoy!" : "Too much!";
+            $scope.validationStateClass = "has-success";
+        }
+    }
+}());
